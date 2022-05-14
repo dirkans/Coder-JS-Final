855ab945a0695832bbf319ef0687bc216e2a665d
Pablo Ambrosio <dirkans@hotmail.comgit config --global user.email dirkans@hotmail.comgit config --global user.email dirkans@hotmail.com>
2022-05-13 23:32:30 -0300

    html encaminado - Comenzando con script

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.write("Hello")
+var carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+totNavBar = document.getElementById("totNavBar");
+
+price1a = document.getElementById("price1"); 
+price2a = document.getElementById("price2");
+price3a = document.getElementById("price3");
+
+add1 = document.getElementById("add1");
+add2 = document.getElementById("add2");
+add3 = document.getElementById("add3");
+
+price1a.innerHTML = 82000;
+price2a.innerHTML = 51499;
+price3a.innerHTML = 11900;
+
+totNavBar.innerHTML = 52000;
+
+
